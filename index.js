45c6ee9584c470da4d8b80227785cd0850da5e7b
herteleo <leonard.hertel@gmail.com>
2019-05-19 23:32:21 +0200

    feat: integrate vuido compiler with webpack config

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-module.exports = () => {};
+const VuidoTemplateCompiler = require('vuido-template-compiler');
+const webpack = require('webpack');
+
+module.exports = (api) => {
+  const vueConfig = api;
+
+  vueConfig.chainWebpack((webpackConfig) => {
+    webpackConfig.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap((loaderOptions) => {
+        const config = loaderOptions;
+
+        config.compiler = VuidoTemplateCompiler;
+        return config;
+      });
+  });
+
+  vueConfig.configureWebpack(() => ({
+    target: 'node',
+    plugins: [
+      new webpack.ExternalsPlugin(
+        'commonjs',
+        ['libui-node'],
+      ),
+    ],
+  }));
+};
