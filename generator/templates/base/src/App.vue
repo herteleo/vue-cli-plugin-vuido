be984cc5c0f13131a265662425d6a2565b277603
herteleo <leonard.hertel@gmail.com>
2019-05-19 23:17:00 +0200

    feat: example template for App.vue

---
+++ generator/templates/base/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Window
+    title="Welcome to your Vuido application!"
+    width="400"
+    height="100"
+    margined
+    @close="exit"
+  >
+    <Box>
+      <Text>
+        Welcome to your Vuido application!
+      </Text>
+    </Box>
+  </Window>
+</template>
+
+<script>
+export default {
+  methods: {
+    exit() {
+      this.$exit();
+    },
+  },
+};
+</script>
